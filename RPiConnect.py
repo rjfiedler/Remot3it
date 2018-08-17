50302bfb3207b5661b2ed9c57a0d0f737c1de7e7
rjfiedler <fiedler.ross@gmail.com>
2018-08-17 10:23:25 -0500

    Initial commit

---
+++ RPiConnect.py
@@ -0,0 +1,47 @@
+import requests, json
+
+def GetProxy():
+    APIKey = 'RDhEOTUyMTUtMDZGOC00NEQ5LThBMEYtMkU5NjBCOUFGQzVB'
+
+    url = "https://api.remot3.it/apv/v23.5/user/login"
+
+    payload = "{ \"username\" : \"fiedlerross94@gmail.com\", \"password\" : \"510Rf327!!\" }"
+    headers = {
+        'developerkey': APIKey,
+        'content-type': "application/json",
+        'cache-control': "no-cache"
+    }
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+    json_response = response.json()
+    token = json_response["token"]
+
+    # gets deviceaddress
+    apiMethod = "https://"
+    apiServer = "api.remot3.it"
+    apiVersion = "/apv/v23.5"
+    deviceListURL = apiMethod + apiServer + apiVersion + "/device/list/all"
+
+    deviceHeaders = {
+        'Content-Type': "application/json",
+        'developerkey': APIKey,
+        'token': token,
+    }
+
+    resp = requests.get(deviceListURL, headers=deviceHeaders)
+    device_list = resp.json()
+    Ross_Pi_001 = device_list["devices"][1]["deviceaddress"]
+
+    # connects to RPi
+    myip = requests.get('http://ip.42.pl/raw').text
+    proxyconnectURL = apiMethod + apiServer + apiVersion + "/device/connect"
+    connect_params = {'deviceaddress': Ross_Pi_001, "wait": 1, "hostip": myip}
+    json_connect_params = json.dumps(connect_params)
+
+    connect_resp = requests.post(proxyconnectURL, headers=deviceHeaders, data=json_connect_params).json()
+    connection = connect_resp["connection"]["proxy"]
+    return connection
+
+
+proxy = GetProxy()
+print(proxy)
